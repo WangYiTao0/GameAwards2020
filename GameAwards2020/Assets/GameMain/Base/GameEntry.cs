e22bcfed6d407f3c0949c1e8de27076e0cf1567c
W <as5818956@gmail.com>
2020-03-14 01:29:52 +0900

    Add　GameMain
    Add Standard Asset (will change)

---
+++ GameAwards2020/Assets/GameMain/Base/GameEntry.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace GameName
+{
+    public partial class GameEntry : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            InitBuiltinComponents();
+            InitCustomComponents();
+            InitCustomDebuggers();
+        }
+    }
+}
