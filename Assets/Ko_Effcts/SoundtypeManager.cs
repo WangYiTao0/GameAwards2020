24aa62a2a1fc128f0ea6b29f86ce37deb01f9275
kosyunrin <53086860+kosyunrin@users.noreply.github.com>
2020-05-23 00:58:00 +0900

    プロトタイプ　ステージ　１

---
+++ Assets/Ko_Effcts/SoundtypeManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundtypeManager :MonoBehaviour
+{
+    [System.Serializable]
+    public struct SOUND_TYPE
+    {
+       public int SoundID;
+        public Sprite SoundImage;
+        public UnityEngine.Events.UnityAction ActionName;//需要触发的能力
+    }
+  protected virtual void SoundAction()
+    {
+
+    }
+}
